44737384f203676bc3d0d3f56d193c55e952007f
mike <t0astt93@gmail.com>
2015-05-26 20:13:46 -0500

    Not sure....

--- app/src/main/java/com/mikerinehart/geekrepublic/fragments/ArticleListFragment.java
+++ app/src/main/java/com/mikerinehart/geekrepublic/fragments/ArticleListFragment.java
@@ -16,7 +16,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -174,6 +173,7 @@ public class ArticleListFragment extends Fragment {
 
             }
         });
+
         getArticles();
         return view;
     }
